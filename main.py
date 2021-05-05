a6e7be0ba3a3a4305a636b55a1e8899bf402db11
NikoriakViktot <83222767+NikoriakViktot@users.noreply.github.com>
2021-05-05 19:01:05 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+
+    # Task1
+
+    def favorite_movie(title):
+        print('Мій улюблений фільм ' + title)
+    favorite_movie('Гая Річчі "Карти, гроші, два ствола"')
+
+    # Task 2
+   # def make_country(country_name, capitals):
+    #   country_name = dict(zip(["Ukraine", "USA", "France"], ["Kiev", "Washington", "Paris"]))
